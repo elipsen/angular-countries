879a7cfe46490fb72d34497c517b4abbbd9d5a25
DESKTOP-01LMNNH\NormandySR1 <jpardal86@gmail.com>
2023-08-26 10:49:48 +0200

    Modificación proyecto y funcionalidad

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'
+import { CountriesService } from '../../services/countries.service.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-country-page',
@@ -6,6 +10,22 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
 
+  public country?: Country;
+
+  constructor( private activatedRoute: ActivatedRoute, private countriesService: CountriesService, private router: Router ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        //switchMap( params => this.countriesService.searchCountryByAlphaCode(params['id']))
+        switchMap( ({id}) => this.countriesService.searchCountryByAlphaCode(id)) // Desestructurando
+      )
+      //.subscribe(params => { // El subscribe cambia el resultado por el switchMap anterior, que modifica el resultado
+      .subscribe( country => {
+        if (!country) return this.router.navigateByUrl('');
+        return this.country = country;
+      })
+  }
 }
--- src/app/countries/services/countries.service.service.ts
+++ src/app/countries/services/countries.service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, catchError } from 'rxjs'
+import { Observable, of, catchError, map } from 'rxjs'
 import { Country } from '../interfaces/country';
 
 const _API_URL: string = 'https://restcountries.com/v3.1';
@@ -11,6 +11,21 @@ const _API_URL: string = 'https://restcountries.com/v3.1';
 export class CountriesService {
 
   constructor( private http: HttpClient) { }
+
+  searchCountryByAlphaCode( code: string): Observable<Country | null> {
+    return this.http.get<Country[]>(`${_API_URL}/alpha/${code}`)
+      .pipe(
+        map(
+          // Incluimos el map del pipe para controlar si devolvemos un pais o null, para evitar devolver un array
+          countries => countries.length > 0 ? countries[0] : null
+        ),
+        catchError( error => {
+           // Capturamos el error, lo mostramos y retornamos vacio para evitar mal funcionamiento
+          console.log(error);
+          return of(null);
+        })
+      );
+  }
 
   searchCapital(term: string): Observable<Country[]> {
     return this.http.get<Country[]>(`${_API_URL}/capital/${term}`)
